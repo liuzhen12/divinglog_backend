27d582a4a47fadf86ec3655711f103442a9cb744
liuzhen12 <liuzhen880215@163.com>
2017-06-06 16:51:02 +0800

    speciality logix

--- controllers/SpecialityController.php
+++ controllers/SpecialityController.php
@@ -14,4 +14,20 @@ use app\components\base\BaseController;
 class SpecialityController extends BaseController
 {
     public $modelClass = 'app\models\Speciality';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        $actions['create'] = [
+            'class' => 'app\actions\speciality\CreateAction',
+            'modelClass' => $this->modelClass,
+            'checkAccess' => [$this, 'checkAccess'],
+        ];
+        $actions['delete'] = [
+            'class' => 'app\actions\speciality\DeleteAction',
+            'modelClass' => $this->modelClass,
+            'checkAccess' => [$this, 'checkAccess'],
+        ];
+        return $actions;
+    }
 }
