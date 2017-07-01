d8a702f126d8d46f5540b1b1ab7e9042f450ef7c
liuzhen12 <liuzhen880215@163.com>
2017-07-01 15:11:41 +0800

    gitignore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=divinglog',
-    'username' => 'root',
-    'password' => 'abcd-1234',
-    'charset' => 'utf8',
-];
