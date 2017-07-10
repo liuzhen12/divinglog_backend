0350e959bdb4ee2091acb54c73d5dd7f1b6236e1
liuzhen12 <liuzhen880215@163.com>
2017-07-10 14:52:30 +0800

    base_location table

--- migrations/M170705082435CreateLocationTable.php
+++ migrations/M170705082435CreateLocationTable.php
@@ -10,17 +10,6 @@ class M170705082435CreateLocationTable extends BaseMigration
     // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
     {
-//        $this->createTable('location', [
-//            'id' => $this->primaryKey(),
-//            'source' => $this->smallInteger()->notNull()->defaultValue(0)->comment('1:diver 2:coach 3:divestore'),
-//            'country' => $this->string(2)->notNull()->defaultValue(''),
-//            'province' => $this->string(45)->notNull()->defaultValue(''),
-//            'city' => $this->string(45)->notNull()->defaultValue(''),
-//            'people_count' => $this->bigInteger()->notNull()->defaultValue(0)->comment(''),
-//            'created_at' => $this->integer(11)->notNull()->defaultValue(0)->comment('创建时间戳'),
-//            'updated_at' => $this->integer(11)->notNull()->defaultValue(0)->comment('更新时间戳'),
-//        ]);
-//        $this->createIndex('uniq_i',"location",['source','na','province','city'],true);
         $this->createTable('location', [
             'id' => $this->primaryKey(),
             'source' => $this->smallInteger()->notNull()->defaultValue(0)->comment('1:diver 2:coach 3:divestore'),
