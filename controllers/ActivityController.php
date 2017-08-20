c888ed9a3b59c130f53a6ce99f59d57a1d005e3f
liuzhen12 <liuzhen880215@163.com>
2017-08-20 15:50:01 +0800

    activity index

--- controllers/ActivityController.php
+++ controllers/ActivityController.php
@@ -8,4 +8,17 @@ class ActivityController extends BaseController
 {
     public $modelClass = 'app\models\Activity';
 
+    public function actions()
+    {
+        $actions = parent::actions();
+        $modelClass = $this->modelClass;
+        $actions['index'] = [
+            'class' => 'app\components\base\BaseIndexAction',
+            'modelClass' => $modelClass,
+            'checkAccess' => [$this, 'checkAccess'],
+            'scenario' => $modelClass::SCENARIO_INDEX
+        ];
+        return $actions;
+    }
+
 }
